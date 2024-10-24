355dd2c50be0d35c8535275845043565b0dbcbe0
Sebastian <joabgiraldo@gmail.com>
2024-10-23 22:47:48 -0500

    Feat: responsive

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -27,24 +27,18 @@ const Carousel = ({ deploys }) => {
 
     return (
         <div className="relative w-full">
-             {/* <div className="absolute z-10 left-0 top-0 w-40 h-full bg-gradient-to-r from-[#18181b] to-transparent pointer-events-none"></div>
-             <div className="absolute z-10 right-0 top-0 w-40 h-full bg-gradient-to-r from-transparent to-[#18181b] pointer-events-none"></div> */}
-            {/* Botón de desplazamiento hacia la izquierda */}
             <button
-                className="absolute -left-5 top-1/2 transform -translate-y-1/2 bg-[#333] rounded-md px-4 py-2 z-30 duration-200 hover:scale-105 active:bg-[#252525]"
+                className="absolute -left-1 sm:-left-5 top-1/2 transform -translate-y-1/2 bg-[#333] rounded-md px-4 py-2 z-30 duration-200 hover:scale-105 active:bg-[#252525]"
                 onClick={scrollLeft}
             >
                 ‹
             </button>
-
-            {/* Contenedor del carrusel */}
             <div
                 className="w-full overflow-x-auto scroll-smooth no-scrollbar"
                 ref={carouselRef}
             >
-               
+
                 <div className="flex gap-4 pb-3 max-w-full h-full">
-                    {/* Tarjetas ajustadas para mostrar completamente */}
                     {deploys.map((deploy, index) => (
                         <div key={index} className="flex-shrink-0 w-[calc(100%-1rem)] md:w-[calc(50%-2rem)] h-full scroll-snap-align-start">
                             <Deploys deploy={deploy} />
@@ -52,10 +46,8 @@ const Carousel = ({ deploys }) => {
                     ))}
                 </div>
             </div>
-
-            {/* Botón de desplazamiento hacia la derecha */}
             <button
-                className="absolute -right-5 top-1/2 transform -translate-y-1/2 bg-[#333] rounded-md px-4 py-2 z-30 duration-200 hover:scale-105 active:bg-[#252525]"
+                className="absolute -right-1 sm:-right-5 top-1/2 transform -translate-y-1/2 bg-[#333] rounded-md px-4 py-2 z-30 duration-200 hover:scale-105 active:bg-[#252525]"
                 onClick={scrollRight}
             >
                 ›
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,17 @@ import { ArrowDownToLine } from "lucide-react";
 const Header = () => {
   return (
     <header className="flex justify-between flex-col items-start sm:flex-row">
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center m-auto">
         <img
           src="https://res.cloudinary.com/dovavvnjx/image/upload/v1713732223/me_pr8bas.jpg"
           alt="Fotografia"
           width={120}
           height={120}
-          className="rounded-xl duration-300 h-40 w-60 object-cover"
+          className="rounded-xl duration-300 h-60 sm:h-40 w-60 object-cover"
         />
       </div>
-      <div className="w-full sm:ps-6  py-3 flex flex-col justify-between gap-1 mt-2 relative">
-        <div>
+      <div className="w-full sm:ps-6  py-3 flex flex-col justify-between items-center sm:items-start gap-1 mt-2 relative">
+        <div className="text-center sm:text-start">
           <h1 className="text-3xl font-bold">Sebastián Giraldo</h1>
 
           <p className="text-base">
@@ -30,9 +30,9 @@ const Header = () => {
           </p>
         </div>
 
-        
 
-        <div>
+
+        <div className="text-center sm:text-start">
           <a className="text-base text-sky-200 opacity-100 hover:underline" href="tel:+57 315 2058712">
             Contactame:  315 2058712
           </a>
@@ -40,16 +40,16 @@ const Header = () => {
         </div>
 
         <div className="flex justify-end absolute -bottom-7 right-0">
-        <a
-          aria-label="Curriculum_Sebastian_Giraldo"
-          rel="noreferrer"
-          target="_blank"
-          download="FullStack Developer.pdf"
-          href="/download/FullStack Developer.pdf"
-          className="opacity-70 text-sm flex items-center gap-2 duration-200 hover:opacity-100"
-        >
-          Download Currículum <ArrowDownToLine size={16} />
-        </a>
+          <a
+            aria-label="Curriculum_Sebastian_Giraldo"
+            rel="noreferrer"
+            target="_blank"
+            download="FullStack Developer.pdf"
+            href="/download/FullStack Developer.pdf"
+            className="opacity-70 text-sm flex items-center gap-2 duration-200 hover:opacity-100"
+          >
+            Download Currículum <ArrowDownToLine size={16} />
+          </a>
         </div>
       </div>
     </header>
